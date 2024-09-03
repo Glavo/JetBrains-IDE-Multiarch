f7afd3157da16b39f7ad3745cbb3b2a7b153c365
Glavo <zjx001202@gmail.com>
2024-09-04 01:15:26 +0800

    Build fsNotifier

--- native/README.md
+++ native/README.md
@@ -1,3 +1,4 @@
 See https://github.com/JetBrains/intellij-community/tree/master/native.
 
 * [LinuxGlobalMenu](./LinuxGlobalMenu): `4e95606697e48c517bcfa92655be7dc627c2be40`
+* [fsNotifier](./fsNotifier): `f93937d167866171179b804c5ee4db735f6c6992`
--- native/build.sh
+++ native/build.sh
@@ -6,11 +6,15 @@ NATIVE_DIR=$(dirname "$(realpath "$0")")
 NATIVE_BUILD_DIR="$NATIVE_DIR/build"
 PROJECT_ROOT=$(dirname "${NATIVE_DIR}")
 PROJECT_BUILD_DIR="$PROJECT_ROOT/build"
+OUTPUT_DIR="$NATIVE_BUILD_DIR/libs"
 
 OS_ARCH=$(uname -m)
 
+CC=${CC:-gcc}
+
 rm -rf "$NATIVE_BUILD_DIR"
 mkdir -p "$NATIVE_BUILD_DIR"
+mkdir -p "$OUTPUT_DIR"
 
 ## libdbusmenu
 LIBDBUSMENU_DIR="$NATIVE_BUILD_DIR/libdbusmenu"
@@ -25,4 +29,10 @@ cd "$NATIVE_DIR"
 
 ## LinuxGlobalMenu
 cmake -DCMAKE_BUILD_TYPE=Release -S "$NATIVE_DIR/LinuxGlobalMenu/" -B "$NATIVE_BUILD_DIR/LinuxGlobalMenu/"
-cmake --build "$NATIVE_BUILD_DIR/LinuxGlobalMenu/"
+cmake --build
+cp "$NATIVE_BUILD_DIR/LinuxGlobalMenu/libdbm.so" "$OUTPUT_DIR/libdbm.so"
+
+## fsNotifier
+$CC -O2 -Wall -Wextra -Wpedantic -D "VERSION=\"f93937d\"" -std=c11 \
+  "$NATIVE_DIR/main.c" "$NATIVE_DIR/inotify.c" "$NATIVE_DIR/util.c" \
+  -o "$OUTPUT_DIR/fsnotifier"
