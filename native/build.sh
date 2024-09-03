a4e53e90ea590c0acafe318a5a933a3c2106d8af
Glavo <zjx001202@gmail.com>
2024-09-04 00:39:42 +0800

    Copy libdbusmenu-glib.a

--- native/build.sh
+++ native/build.sh
@@ -9,15 +9,18 @@ PROJECT_BUILD_DIR="$PROJECT_ROOT/build"
 
 OS_ARCH=$(uname -m)
 
+rm -rf "$NATIVE_BUILD_DIR"
 mkdir -p "$NATIVE_BUILD_DIR"
 
 ## libdbusmenu
 LIBDBUSMENU_DIR="$NATIVE_BUILD_DIR/libdbusmenu"
-rm -rf "$LIBDBUSMENU_DIR"
 git clone https://github.com/JetBrains/libdbusmenu.git "$LIBDBUSMENU_DIR"
 cd "$LIBDBUSMENU_DIR"
 git checkout 38d7a2ada4b2a08c535491d43a39825868f2b065
 ./configure --build "$OS_ARCH-unknown-linux-gnu" --target "$OS_ARCH-unknown-linux-gnu"
 cd "$LIBDBUSMENU_DIR/libdbusmenu-glib"
 make
+cp "$LIBDBUSMENU_DIR/libdbusmenu-glib/.libs/libdbusmenu-glib.a" "$NATIVE_DIR/LinuxGlobalMenu/"
 cd "$NATIVE_DIR"
+
+## LinuxGlobalMenu
