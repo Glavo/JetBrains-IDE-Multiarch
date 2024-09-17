bad4d750117f3823b40789931e03dded9a8b5622
Glavo <zjx001202@gmail.com>
2024-09-18 00:32:26 +0800

    Create Product::getDownloadLink

--- buildSrc/src/main/java/org/glavo/build/Product.java
+++ buildSrc/src/main/java/org/glavo/build/Product.java
@@ -1,13 +1,22 @@
 package org.glavo.build;
 
 public enum Product {
-    INTELLIJ_COMMUNITY_EDITION("IC"),
-    INTELLIJ_ULTIMATE("IU");
+    INTELLIJ_COMMUNITY_EDITION("IC", "idea", "ideaIC"),
+    INTELLIJ_ULTIMATE("IU", "idea", "ideaIU"),
+    GOLAND("GO", "go", "goland");
 
     private final String code;
+    private final String downloadLinkPrefix;
+    private final String fileNameBase;
 
-    Product(String code) {
+    Product(String code, String downloadLinkPrefix, String fileNameBase) {
         this.code = code;
+        this.downloadLinkPrefix = downloadLinkPrefix;
+        this.fileNameBase = fileNameBase;
+    }
+
+    public String getDownloadLink(String version, Arch arch) {
+        return "https://download.jetbrains.com/%s/%s-%s-%s.tar.gz".formatted(downloadLinkPrefix, fileNameBase, version, arch.normalize());
     }
 
     public String getCode() {
